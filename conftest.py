b484afebab85352ec8e5c2f67941460bfade15f8
Екатерина Кокшарова <e.koksharova@ticketland.ru>
2019-07-31 18:49:11 +0300

    fixed language parmeter script

--- conftest.py
+++ conftest.py
@@ -1,9 +1,10 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
+    parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default=None,
                      help="Choose language: ru, en, ... (etc.)")
@@ -12,7 +13,7 @@ def pytest_addoption(parser):
 def browser(request):
     browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language") 
-    browser = None
+    
     if browser_name == "chrome":
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
@@ -26,11 +27,8 @@ def browser(request):
         browser = webdriver.Firefox(firefox_profile=fp,
             executable_path='C:\\Users\\e.koksharova\\Documents\\geckodriver.exe',
             firefox_binary='C:\\Users\\e.koksharova\\AppData\\Local\\Mozilla Firefox\\firefox.exe')
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+
     yield browser
     print("\nquit browser..")
     browser.quit()
-
-
     
