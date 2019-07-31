f1df382713bc6e627b173a15ea02c99dcfa7f860
Екатерина Кокшарова <e.koksharova@ticketland.ru>
2019-07-31 18:53:43 +0300

    added test_check_basket_button_exists

--- test_items.py
+++ test_items.py
@@ -1,11 +1,6 @@
-import pytest
 import time
-from selenium import webdriver
 
-link = "http://selenium1py.pythonanywhere.com/"
-
-
-def test_guest_should_see_login_link(browser):
-    browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
+def test_check_basket_button_exists(browser):
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')     
+    assert len(browser.find_elements_by_css_selector('.btn-add-to-basket')) == 1
     time.sleep(5)
