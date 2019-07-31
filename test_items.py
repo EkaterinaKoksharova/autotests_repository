17f869418226fdbc60df878c40092aa2f52448f2
Екатерина Кокшарова <e.koksharova@ticketland.ru>
2019-07-31 15:03:28 +0300

    created test_items file

---
+++ test_items.py
@@ -0,0 +1,11 @@
+import pytest
+import time
+from selenium import webdriver
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#login_link")
+    time.sleep(5)
